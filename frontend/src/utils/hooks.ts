a8a96d2aab605f2446b339286657d317732cb800
Yurii <lemyxx@gmail.com>
2024-12-02 13:41:43 +0200

    Added function to auto reconnect to websockets when connection was lost (for example, after wake up of laptop from sleep).
    Added gradual change of fan speed towards the target speed.

--- frontend/src/utils/hooks.ts
+++ frontend/src/utils/hooks.ts
@@ -2,32 +2,74 @@ import { useState, useEffect } from 'react';
 import { toast } from 'react-toastify';
 import { errorToastStyle } from './misc';
 
-export function useWebSocket(onMessage: WebSocket['onmessage']) {
+export function useWebSocket(onMessage: WebSocket['onmessage'])
+{
   const [ws, setWs] = useState<WebSocket>();
 
-  useEffect(() => {
-    const _ws = new WebSocket('ws://localhost:5522');
+  useEffect(() =>
+  {
+    var retryCount = 0;
+    const maxRetryCount = 50;
+    const reconnectTimeout = 5;
+    var isReconnecting = false;
+    var _ws: WebSocket;
 
-    _ws.onmessage = onMessage;
-
-    _ws.onopen = () => {
-      setWs(_ws);
-    };
-
-    _ws.onclose = () => {
-      toast.error('WebSocket connection was lost. Please refresh the page.', {
-        className: errorToastStyle,
-        autoClose: false,
+    function onOpen()
+    {
+      toast.success('WS is OPEN', {
         closeButton: false,
-        hideProgressBar: true,
+        hideProgressBar: false,
         draggable: false,
-        closeOnClick: false,
+        closeOnClick: true,
+        autoClose: 5000
       });
-    };
+      retryCount = 0;
+      setWs(_ws);
+    }
 
-    return () => {
+    function onErrorOrOnClose()
+    {
+      var message = 'WebSocket connection was not established or lost.';
+      if(retryCount < maxRetryCount)
+      {
+        message += ' Reconnect retry #' + retryCount + ' of ' + maxRetryCount;
+      }
+      toast.error(message, {
+        className: errorToastStyle,
+        autoClose: 5000,
+        closeButton: false,
+        hideProgressBar: false,
+        draggable: false,
+        closeOnClick: false
+      });
+
+      if(!isReconnecting && retryCount < maxRetryCount)
+      {
+        isReconnecting = true;
+        setTimeout(() =>
+        {
+          retryCount++;
+          openWebsocket();
+          isReconnecting = false;
+        }, reconnectTimeout * 1000);
+      }
+    }
+
+    function openWebsocket()
+    {
+      _ws = new WebSocket('ws://localhost:5522');
+      _ws.onmessage = onMessage;
+      _ws.onopen = onOpen;
+      _ws.onclose = onErrorOrOnClose;
+      _ws.onerror = onErrorOrOnClose;
+    }
+
+    openWebsocket();
+
+    return () =>
+    {
       _ws.close();
-    };
+    }
   }, [onMessage]);
 
   return ws;
